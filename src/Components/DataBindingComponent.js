90ba6f0d37342e194850d1f6ce3031c5fa3382ee
Iamam786 <93914102+Iamam786@users.noreply.github.com>
2023-03-13 10:41:59 +0530

    DAtaBindigFake files and databindg update

--- src/Components/DataBindingComponent.js
+++ src/Components/DataBindingComponent.js
@@ -5,19 +5,65 @@ export default function DataBindingComponent() {
     let Price = 50000;
     let Stock = true;
     let Categories = ['Electronic', 'Footwear', 'Fashion'];
-    let product = {"id":1,"title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men's clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}}
+    let product = { "id": 1, "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", "price": 109.95, "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday", "category": "men's clothing", "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", "rating": { "rate": 3.9, "count": 120 } };
+    let ArrayofProduct = [
+        { Name: 'Samsung tv', Price: 4000, Stock: true },
+        { Name: 'Nike Casulas', Price: 4830, Stock: false },
+        { Name: 'Samsung tv', Price: 4000, Stock: true },
+    ]
     return (
         <>
-    <h2>Object data Binding</h2>
-    <h2>productl list</h2>
-        <dl>
-            <dt>id</dt>
-            <dd>{product.id}</dd>
-            <dt>title</dt>
-            <dd>{product.title}</dd>
-            <dt>image</dt>
-            <dd><img src="{product.image}"></img></dd>
-        </dl>
+
+            <h2>Array Of product</h2>
+            <table className="table">
+                <thead>
+
+                    <tr className="">
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+                        <th>Actions</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        ArrayofProduct.map(prod =>
+                            <>
+                                <tr>
+                                    <td>{prod.Name}</td>
+                                    <td>{prod.Price}</td>
+                                    <td>{prod.Stock === true ? "Avilable" : "Out Of Stock "}</td>
+                                    <td>
+                                        <button className="btn btn-primary"><span className="bi bi-eye-fill"></span></button>
+                                    </td>
+                                    <td>
+                                        <button className="btn btn-danger"><span className="bi bi-trash"></span></button>
+                                    </td>
+
+
+                                </tr>
+                            </>
+                        )
+                    }
+                </tbody>
+            </table>
+
+
+
+            <h2>Object data Binding</h2>
+            <h2>productl list</h2>
+            <dl>
+                <dt>id</dt>
+                <dd>{product.id}</dd>
+                <dt>title</dt>
+                <dd>{product.title}</dd>
+                <dt>image</dt>
+                <dd><img src={product.image} width="200" height="200" alt="loding"></img></dd>
+                <dt>Rating</dt>
+                <dd><span className="bi bi-star-fill text-primary"></span></dd>
+
+            </dl>
 
             <h2>Categories Table</h2>
             <table className="table table-dark">
@@ -65,7 +111,7 @@ export default function DataBindingComponent() {
 
 
                 {Categories.map(category =>
-                    <option key={category}>{category}</option>
+                    <li key={category}><input type="checkbox"></input><span>{category}</span></li>
                 )
                 }
 
--- src/index.js
+++ src/index.js
@@ -7,21 +7,18 @@ import { LoginComponent } from './Components/loginComponents';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import { NetflixRegisterComponent } from './Components/NetflixComponentRegis';
+import DataBindingComponent from './Components/DataBindingComponent';
+import DataBindingFake from './Components/DataBindingFake';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className='d-flex justify-content-center bimage   text-center flex-column text-white'>
+    <DataBindingFake />
+    <DataBindingComponent></DataBindingComponent>
 
-   
-    <h1 className=' '>Unlimited movies, TV shows and more</h1>
-    <p className=''>watch anywhere. Cancel anytime</p>
-    <NetflixRegisterComponent></NetflixRegisterComponent>
-    </div>
-  
   </React.StrictMode>
 );
-         
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
