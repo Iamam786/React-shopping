3ff0d709a43ebbc2669d6898ced543ecf65e842b
Iamam786 <93914102+Iamam786@users.noreply.github.com>
2023-04-17 14:48:57 +0530

    context details update form

--- src/Components/Context/ContextDemoComponent.js
+++ src/Components/Context/ContextDemoComponent.js
@@ -3,38 +3,65 @@ import React, { useContext, useState } from 'react'
 let userDetailsContext = React.createContext(null);
 
 export default function ContextDemoComponent() {
-  
-    const [userDetails] = useState({
-        UserName:'abdul',
-        Email:'abdul@gmail.com'
+
+    const [userDetails, setuserDetails] = useState({
+        UserName: '',
+        Email: ''
     });
 
+    function HandleUserName(e) {
+        setuserDetails({
+            UserName: e.target.value,
+            Email: userDetails.Email
+        })
+    }
+    function HandleEmail(e) {
+        setuserDetails({
+            UserName: userDetails.UserName,
+            Email: e.target.value
+        })
+    }
+    function HandleLogin() {
+        setuserDetails({
+            UserName: userDetails.UserName,
+            Email: userDetails.Email
+        })
+    }
+
     return (
-    <userDetailsContext.Provider value={userDetails}>
-      <div className='container-fluid' style={{height:'400px',color:'red'}}>
-        <h2>
-            Main Components - User: {userDetails.UserName}
-        </h2>
-        <HomeComponent/>
-      </div>
-    </userDetailsContext.Provider>
-  )
+        <userDetailsContext.Provider value={userDetails}>
+            <div className='container-fluid' style={{ height: '400px', color: 'red' }}>
+                <h1>User Login</h1>
+                <dl>
+                    <dt>User Name</dt>
+                    <dd><input type='text' onChange={HandleUserName} /></dd>
+                    <dt>Email</dt>
+                    <dd><input type='email' onChange={HandleEmail} /></dd>
+                    <button className='btn btn-primary' onClick={HandleLogin} >Login</button>
+                </dl>
+                <h2>
+                    Main Components - User: {userDetails.UserName}
+                </h2>
+                <HomeComponent />
+            </div>
+        </userDetailsContext.Provider>
+    )
 }
 
 function HomeComponent() {
     let userdetails = useContext(userDetailsContext);
-    return(
+    return (
         <div>
             Home Components - {userdetails.UserName}
-            <NavbarComponent/>
+            <NavbarComponent />
         </div>
-        
+
     )
 }
 
 function NavbarComponent() {
     let userdetails = useContext(userDetailsContext)
-    return(
+    return (
         <div className='btn-toolbar bg-dark justify-content-between px-1'>
             <div className='btn-group'>
                 <h3>Navbar</h3>
